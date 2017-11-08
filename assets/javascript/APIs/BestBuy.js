be3c6286f8187543ac81f635c4e6b06f656b0be6
Mark Kazanski <markkazanski@gmail.com>
2017-11-07 21:09:18 -0600

    Added class

--- assets/javascript/APIs/BestBuy.js
+++ assets/javascript/APIs/BestBuy.js
@@ -1,30 +1,34 @@
-var BestBuyAPI = {
-    firstProduct:{
-        name:"",
-        imageURL:"",
-        description:"",
-        releaseDate:"",
-        productRating:"",
-        reviewsNum:"",
-        UPC:"",
-    },
-    searchProduct: function(searchQuery){
-        var requestURL = `https://api.bestbuy.com/v1/products((search=${searchQuery})&customerReviewCount>=1)?apiKey=RRDFqF9yMBmlBXsaQx1TqHe6&sort=customerReviewCount.dsc&pageSize=1&format=json`; 
-       var requestURL2 = `https://api.bestbuy.com/v1/products(upc=${searchQuery})?apiKey=RRDFqF9yMBmlBXsaQx1TqHe6&format=json`;
+const BestBuyAPI = (function() {}());
+
+
+class BestBuyAPI {
+    searchProduct(searchQuery) {
+        //const requestURL = `https://api.bestbuy.com/v1/products((search=${searchQuery})&customerReviewCount>=1)?apiKey=RRDFqF9yMBmlBXsaQx1TqHe6&sort=customerReviewCount.dsc&pageSize=1&format=json`;
+        const requestURL = `https://api.bestbuy.com/v1/products(upc=${searchQuery})?apiKey=RRDFqF9yMBmlBXsaQx1TqHe6&format=json`;
+        var firstProduct = {
+            name: "",
+            imageURL: "",
+            description: "",
+            releaseDate: "",
+            productRating: "",
+            reviewsNum: "",
+            UPC: "",
+        };
 
         $.ajax({
-            url: requestURL2,
+            url: requestURL,
             method: "GET"
-        }).done(function(response) {
-            BestBuyAPI.firstProduct.name = response.products[0].name;
-            BestBuyAPI.firstProduct.imageURL = response.products[0].image;
-            BestBuyAPI.firstProduct.description = response.products[0].longDescription;
-            BestBuyAPI.firstProduct.releaseDate = response.products[0].startDate;
-            BestBuyAPI.firstProduct.productRating = response.products[0].customerReviewAverage;
-            BestBuyAPI.firstProduct.reviewsNum = response.products[0].customerReviewCount;
-            BestBuyAPI.firstProduct.UPC = response.products[0].upc;  
-                     
+        }).done(function (response) {
+            firstProduct.name = response.products[0].name;
+            firstProduct.imageURL = response.products[0].image;
+            firstProduct.description = response.products[0].longDescription;
+            firstProduct.releaseDate = response.products[0].startDate;
+            firstProduct.productRating = response.products[0].customerReviewAverage;
+            firstProduct.reviewsNum = response.products[0].customerReviewCount;
+            firstProduct.UPC = response.products[0].upc;
+
         });
-        return BestBuyAPI.firstProduct;
-    },
-};
+        
+        return firstProduct;
+    }
+}
