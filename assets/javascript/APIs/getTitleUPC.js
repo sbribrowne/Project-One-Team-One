c5062142644ba7e34abe6fdffbbfea96183beb7a
JQuinnie <jenn8866@gmail.com>
2017-11-07 20:20:37 -0600

    adding getTitleUPC html and js files

---
+++ assets/javascript/APIs/getTitleUPC.js
@@ -0,0 +1,25 @@
+function getTitleUPC(str) {
+    if (!str) 
+        return; 
+
+    const requestURL = `http://api.walmartlabs.com/v1/search?query=${searchQuery}&format=json&apiKey=rqwj3h4qjprfdnuzvsn3cz4m`; 
+    var result = {title: "", UPC: "" };
+
+    // ===============CORS CODE=========================================
+    jQuery.ajaxPrefilter(function(options) {
+        if (options.crossDomain && jQuery.support.cors) {
+            options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+        }
+    });
+    // =================================================================        
+
+    $.ajax({
+        url: requestURL,
+        method: "GET"
+    }).done(function(response) {
+        result.title = response.items[0].name;
+        result.UPC = response.items[0].upc;                    
+    });
+
+    return result;
+}
