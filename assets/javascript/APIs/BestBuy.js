9d36bd101920a627a0e74e4719b74b33c070fe8a
Mark Kazanski <markkazanski@gmail.com>
2017-11-07 21:30:23 -0600

    gitignore

--- assets/javascript/APIs/BestBuy.js
+++ assets/javascript/APIs/BestBuy.js
@@ -1,6 +1,3 @@
-const BestBuyAPI = (function() {}());
-
-
 class BestBuyAPI {
     searchProduct(searchQuery) {
         //const requestURL = `https://api.bestbuy.com/v1/products((search=${searchQuery})&customerReviewCount>=1)?apiKey=RRDFqF9yMBmlBXsaQx1TqHe6&sort=customerReviewCount.dsc&pageSize=1&format=json`;
@@ -9,10 +6,6 @@ class BestBuyAPI {
             name: "",
             imageURL: "",
             description: "",
-            releaseDate: "",
-            productRating: "",
-            reviewsNum: "",
-            UPC: "",
         };
 
         $.ajax({
@@ -22,11 +15,6 @@ class BestBuyAPI {
             firstProduct.name = response.products[0].name;
             firstProduct.imageURL = response.products[0].image;
             firstProduct.description = response.products[0].longDescription;
-            firstProduct.releaseDate = response.products[0].startDate;
-            firstProduct.productRating = response.products[0].customerReviewAverage;
-            firstProduct.reviewsNum = response.products[0].customerReviewCount;
-            firstProduct.UPC = response.products[0].upc;
-
         });
         
         return firstProduct;
