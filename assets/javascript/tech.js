3b930b592be50ac47dc6e2d8d8d21d2805682c4b
Alize Nguyen <alizenguyen@gmail.com>
2017-11-09 19:34:04 -0600

    modal add

--- assets/javascript/tech.js
+++ assets/javascript/tech.js
@@ -15,3 +15,11 @@
 $(document).ready(function() {
     $('.carousel').carousel();
   });
+
+//Modal Function
+
+  $(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
+       
